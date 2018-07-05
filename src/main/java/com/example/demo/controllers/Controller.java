6c7c40b260e574015989dfef9b950e7dd8714033
cwannberg <ceciliawannberg@gmail.com>
2018-07-05 13:45:04 +0200

    Tried IoC and DI, started testing

--- src/main/java/com/example/demo/controllers/Controller.java
+++ src/main/java/com/example/demo/controllers/Controller.java
@@ -1,84 +1,49 @@
 package com.example.demo.controllers;
 
+import com.example.demo.MessageStoreInterface;
 import com.example.demo.Model.TextMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 public class Controller {
-
-    private ArrayList<TextMessage> textMessages = new ArrayList<>();
-    //AtomicInteger = An int value that may be updated atomically.
-    private AtomicInteger nextId = new AtomicInteger();
     @Autowired
-    TextMessage textMessage;
+    MessageStoreInterface messageStoreInterface;
+
 
     @PostMapping("/textmessages")
     //Specifies that a new TextMessage is created. Instead of HttpStatus 200 we get 201.
     @ResponseStatus(HttpStatus.CREATED)
     public TextMessage writeNewTextMessage(@RequestBody TextMessage textMessage) {
-
-        this.textMessage = textMessage;
-        //Set TextMessage to have next unique ID:
-        textMessage.setId(nextId.incrementAndGet());
-
-        textMessages.add(textMessage);
-        return textMessage;
+        return messageStoreInterface.writeNewTextMessage(textMessage);
     }
 
     @GetMapping("/textmessages")
     public ArrayList<TextMessage> getAllTextMessages() {
-
-        return textMessages;
+        return messageStoreInterface.getAllTextMessages();
     }
 
     @GetMapping("/textmessages/{id}")
     public TextMessage getTextMessage(@PathVariable("id") int textId) {
-        for (TextMessage textMessage : textMessages) {
-            if (textMessage.getId() == textId) {
-                return textMessage;
-            }
-        }
-        //Instead of just returning null I want to specify what's gone wrong.
-        throw new IllegalArgumentException();
+        return messageStoreInterface.getTextMessage(textId);
     }
 
     @GetMapping("/textmessages/name={name}")
     public TextMessage getTextMessage(@PathVariable("name") String name) {
-        for (TextMessage textMessage : textMessages) {
-            if (textMessage.getName().equals(name)) {
-                return textMessage;
-            }
-        }
-        throw new IllegalArgumentException();
+        return messageStoreInterface.getTextMessage(name);
     }
 
     @PostMapping("/textmessages/{id}")
     public TextMessage editTextMessage(@PathVariable("id") int textId,
                                        @RequestBody TextMessage newTextMessage) {
-        for (TextMessage textMessage : textMessages) {
-            if (textMessage.getId() == textId) {
-                textMessages.remove(textMessage);
-                newTextMessage.setId(textId);
-                textMessages.add(newTextMessage);
-                return textMessage;
-            }
-        }
-        throw new IllegalArgumentException();
+        return messageStoreInterface.editTextMessage(textId, newTextMessage);
     }
 
     @DeleteMapping("/textmessages/{id}")
     public TextMessage removeTextMessage(@PathVariable("id") int textId) {
-        for (TextMessage textMessage : textMessages) {
-            if (textMessage.getId() == textId) {
-                textMessages.remove(textMessage);
-                return textMessage;
-            }
-        }
-        throw new IllegalArgumentException();
+        return messageStoreInterface.removeTextMessage(textId);
     }
 }
